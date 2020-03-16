b673eb7587d624e2f93b8da0d9be86ccf7b3f007
Jacob Zirbel <jzerble@gmail.com>
2020-03-16 02:47:30 -0500

    pick character/pick enemy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 game = {
 	DOMElements: {
 		youWrapper: $(".youWrapper"),
+		enemyWrapper: $(".enemyWrapper"),
 		fighters: $(".fighters"),
 		fighter1: $("#fighter1"),
 		fighter2: $("#fighter2"),
@@ -9,24 +10,47 @@ game = {
 	},
 	currentCharacter: {},
 	currentEnemy: {},
-	fighters: {
-		F1: { HP: 10, attack: 4, counter: 4 },
-		F2: { HP: 10, attack: 4, counter: 4 },
-		F3: { HP: 10, attack: 4, counter: 4 },
-		F4: { HP: 10, attack: 4, counter: 4 }
-	},
+	fighters: [
+		{ name: "fighter1", HP: 10, attack: 4, counter: 4 },
+		{ name: "fighter2", HP: 10, attack: 4, counter: 4 },
+		{ name: "fighter3", HP: 10, attack: 4, counter: 4 },
+		{ name: "fighter4", HP: 10, attack: 4, counter: 4 }
+	],
 	testAlert: () => alert("hello"),
-	moveFighter(f) {
+	selectFighter(f) {
 		f.hide();
 		this.DOMElements.youWrapper.append(f);
 		f.show();
+		this.currentCharacter = this.fighters.find(e => f.attr("id") === e.name);
+		this.fighters = this.fighters.filter(e => f.attr("id") !== e.name);
+		console.log(this.fighters);
 	},
-	run() {
-		$(".fighter").on("click", function() {
-			game.moveFighter($(this));
+	selectEnemy(f) {
+		f.hide();
+		this.DOMElements.enemyWrapper.append(f);
+		f.show();
+		this.currentEnemy = this.fighters.find(e => f.attr("id") === e.name);
+		this.fighters = this.fighters.filter(e => f.attr("id") !== e.name);
+		console.log(this.fighters);
+	},
+	pickCharacter() {
+		$(".option").on("click", function() {
+			$(".option").off();
+			$(this).attr("class", "yourFighter fighter");
+			console.log(this);
+			game.selectFighter($(this));
+			game.pickEnemy();
+		});
+	},
+	pickEnemy() {
+		$(".option").on("click", function() {
+			$(".option").off();
+			$(this).attr("class", "enemyFighter fighter");
+			console.log(this);
+			game.selectEnemy($(this));
 		});
 	}
 };
 $(document).ready(() => {
-	game.run();
+	game.pickCharacter();
 });
